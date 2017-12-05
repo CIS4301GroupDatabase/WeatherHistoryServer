a806d5f3c2a7d081dd86fad050921bdc5863b27d
Nuchaz <nuchaz.ipod@gmail.com>
2017-12-05 03:43:24 -0500

    Bunch of fixes and work to make queries format json correctly

--- src/main/java/servlet/WeatherServlet.java
+++ src/main/java/servlet/WeatherServlet.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -14,8 +16,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.cedarsoftware.util.io.JsonWriter;
-import com.ohadr.common.utils.JsonUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import weatherhistory.server.Config;
 import weatherhistory.server.DatabaseService;
@@ -61,15 +63,14 @@ public class WeatherServlet extends HttpServlet
 		if (request.getQueryString() != null && (request.getQueryString().contains("sql") || request.getQueryString().contains("data")))
 		{
 			String sqlQuery;
-			boolean makePretty = false;
 			if (request.getQueryString().contains("sql"))
 			{
 				sqlQuery = request.getParameter("sql");
+				sqlQuery = sqlQuery.replace(";", "");
 				out.println("SQL Query:");
 				out.println(sqlQuery);
 				out.println();
 				out.println("Result:");
-				makePretty = true;
 			}
 			else
 			{
@@ -85,17 +86,9 @@ public class WeatherServlet extends HttpServlet
 					Statement query = connect.createStatement();
 					System.out.println("Sending query");
 					ResultSet result = query.executeQuery(sqlQuery);
-					String json = JsonUtils.convertResultSetToJson(result); 
-					if (makePretty)
-					{
-						// print a fully formated json string
-						out.print(JsonWriter.formatJson(json));
-					}
-					else
-					{
-						// print the json as a single line
-						out.print(json);
-					}
+					
+					String json = convertResultSetToJson(result); 
+					out.print(json);
 					System.out.println("Response recieved, disconnecting from database.");
 					database.disconnectFromDatabase(connect);
 				} catch (SQLException e) 
@@ -116,4 +109,41 @@ public class WeatherServlet extends HttpServlet
 		out.close();
 	}
 	
+	public String quoteDatesAndTimes(String input)
+	{
+		String output = input;
+		//output = output.re how in the world.
+		return output;
+	}
+	
+	@SuppressWarnings("unchecked")	
+	public static String convertResultSetToJson(ResultSet resultSet) throws SQLException
+	{
+		if(resultSet == null)
+			return null;
+		
+		JSONArray json = new JSONArray();
+		ResultSetMetaData metadata = resultSet.getMetaData();
+		int numColumns = metadata.getColumnCount();
+		
+		while(resultSet.next()) 			//iterate rows
+		{
+			JSONObject obj = new JSONObject();		//extends HashMap
+			for (int i = 1; i <= numColumns; ++i) 			//iterate columns
+			{
+				String column_name = metadata.getColumnName(i);
+				Object data = resultSet.getObject(column_name);
+				if (column_name.contentEquals("CONDITION_DATE") || column_name.contentEquals("CONDITON_DATE") || column_name.contentEquals("SUNRISE_TIME") ||column_name.contentEquals("SUNSET_TIME"))
+				{
+					Timestamp time = (Timestamp) data;
+					String date = time.toString();
+					data = date;
+				}
+				obj.put(column_name, data);
+			}
+			json.add(obj);
+		}
+		return json.toJSONString();
+	}
+
 }
